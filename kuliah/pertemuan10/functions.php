59140bcc6776c5c5d9a3db7a512afdb654014027
bynurhalim <bynurhalim@gmail.com>
2022-03-16 22:08:27 +0800

    menambahkan fitur insert data

--- kuliah/pertemuan10/functions.php
+++ kuliah/pertemuan10/functions.php
@@ -24,3 +24,30 @@ function query($query)
   }
   return $rows;
 }
+
+function tambah($data)
+{
+  // menghubungkan database
+  $conn = koneksi();
+
+  // membuat variabel untuk membantu di perintah query
+  $nama = htmlspecialchars($data['nama']);
+  $nrp = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO 
+          mahasiswa
+          VALUES
+          (null, '$nama', '$nrp', '$$email', '$jurusan', '$gambar');
+          ";
+  // untuk menjalankan perintah fungsi tambah() dengan perintah query dan bisa terhubung ke database
+  mysqli_query($conn, $query);
+
+  // untuk menunjukkan error nya dimana
+  echo mysqli_error($conn);
+
+  // jika fungsi tambah() berhasil, maka akan keluar nilai 1
+  return mysqli_affected_rows($conn);
+}
