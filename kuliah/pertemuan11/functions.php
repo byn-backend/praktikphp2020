fd5f72cb94606e4384e91a4d2be696938db84bdb
bynurhalim <bynurhalim@gmail.com>
2022-03-17 16:31:44 +0800

    menambahkan fungsi hapus, ubah dan pencarian

---
+++ kuliah/pertemuan11/functions.php
@@ -0,0 +1,111 @@
+<?php
+function koneksi()
+{
+  // koneksi DB dan pilih database
+  return mysqli_connect('localhost', 'root', '', 'pw_043040023');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  // Query isi tabel mahasiswa
+  $result = mysqli_query($conn, $query);
+
+  // jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  // ubah data ke dalam array, 
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function tambah($data)
+{
+  // menghubungkan database
+  $conn = koneksi();
+
+  // membuat variabel untuk membantu di perintah query
+  $nama = htmlspecialchars($data['nama']);
+  $nrp = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO 
+          mahasiswa
+          VALUES
+          (null, '$nama', '$nrp', '$$email', '$jurusan', '$gambar');
+          ";
+  // untuk menjalankan perintah fungsi tambah() dengan perintah query dan bisa terhubung ke database
+  mysqli_query($conn, $query);
+
+  // untuk menunjukkan error nya dimana
+  echo mysqli_error($conn);
+
+  // jika fungsi tambah() berhasil, maka akan keluar nilai 1
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = Koneksi();
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = Koneksi();
+
+  // membuat variabel untuk membantu di perintah query
+  $id = htmlspecialchars($data['id']);
+  $nama = htmlspecialchars($data['nama']);
+  $nrp = htmlspecialchars($data['nrp']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET 
+            nama = '$nama',
+            nrp = '$nrp',
+            email = '$email',
+            jurusan = '$jurusan',
+            gambar = '$gambar'
+            
+            WHERE id = '$id';
+                    ";
+  // untuk menjalankan perintah fungsi tambah() dengan perintah query dan bisa terhubung ke database
+  mysqli_query($conn, $query);
+
+  // untuk menunjukkan error nya dimana
+  echo mysqli_error($conn);
+
+  // jika fungsi tambah() berhasil, maka akan keluar nilai 1
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = Koneksi();
+  $query = ("SELECT * FROM mahasiswa 
+  WHERE 
+  nama LIKE '%$keyword%' OR
+  nrp LIKE '%$keyword%'
+  
+  ");
+
+  $result =  mysqli_query($conn, $query);
+  // ubah data ke dalam array, 
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
